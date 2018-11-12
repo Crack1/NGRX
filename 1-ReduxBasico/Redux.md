23f3c38003e46b0e27a6e4dfb32aa91ebb2bd11e
Erwin Vides <erwinvides@hotmail.com>
2018-11-11 21:11:35 -0600

    NGRX Angular

---
+++ 1-ReduxBasico/Redux.md
@@ -0,0 +1,12 @@
+TS NODE sirve para correr typescrypt en node
+https://github.com/TypeStrong/ts-node
+npm install -D ts-node
+npm install -D typescript
+
+Para Ejecutar se si es instalado de forma global usa  ts-node app-1.ts
+Para ejecutar si se instala de forma dev se use ./node_modules/.bin/ts-node app-1.ts
+
+
+Una Action solo sirve para describir algo que deberia de hacer el reducer
+
+Interface /*es una regla que un objeto debe de cumplir*/
---
+++ 1-ReduxBasico/app-1.ts
@@ -0,0 +1,52 @@
+interface Action {
+  type: string
+  payload?: any
+}
+
+/* const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+} */
+
+function reducer(state = 10, action: Action) {
+
+  /*   if (action.type === 'INCREMENTAR') {
+      return state++
+    } */
+
+  switch (action.type) {
+    case 'INCREMENTAR':
+      return state += 1
+    case 'DECREMENTAR':
+      return state -= 1
+    case 'MULTIPLICAR':
+      return state * action.payload
+    case 'DIVIDIR':
+      return state / action.payload
+    default:
+      break;
+  }
+
+  return state
+}
+//Usar el Reducer
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+}
+console.log(reducer(10, incrementadorAction))
+
+const decrementadorAction: Action = {
+  type: 'DECREMENTAR'
+}
+console.log(reducer(10, decrementadorAction))
+
+const multiplicarAction: Action = {
+  type: 'MULTIPLICAR',
+  payload: 2
+}
+console.log(reducer(10, multiplicarAction))
+
+const dividirAction: Action = {
+  type: 'DIVIDIR',
+  payload: 2
+}
+console.log(reducer(10, dividirAction))
